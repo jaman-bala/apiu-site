d183971580545657673bde6f69f60a178386247f
Dastan <ermekoffdastan@gmail.com>
2022-11-18 18:01:54 +0600

    add models

--- online/models.py
+++ online/models.py
@@ -4,3 +4,15 @@ class Online(models.Model):
     first_name = models.CharField("Имя", max_length=50)
     last_name = models.CharField("Фамилия", max_length=50)
     email = models.EmailField("Почта")
+    title = models.CharField("Название", max_length=50)
+    text = models.TextField("Описание")
+    file = models.FileField(upload_to='file', blank=True, null=True)
+    date = models.DateTimeField("Дата публикации")
+
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        verbose_name = 'Онлайн заявка'
+        verbose_name_plural = 'Онлайн заявки'    
--- online/views.py
+++ online/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Online
 # Create your views here.
+
+
+def online(request):
+    online = Online.objects.all()
+    return render(request, 'online/online.html', {'online': online} )
