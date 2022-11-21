490160f3d6a9c1479d2b2fbbed824742a09c9aae
Dastan <ermekoffdastan@gmail.com>
2022-11-21 17:42:57 +0600

    add new html

--- cloude_apiu/settings.py
+++ cloude_apiu/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'cloude_apiu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,13 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- news/views.py
+++ news/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from .models import Articles
-# Create your views here.
+from django.views.generic import DetailView
+
 
 
 def news_home(request):
-    news = Articles.objects.all()
+    news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news} )
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
