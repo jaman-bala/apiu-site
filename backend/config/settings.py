6c0a26d01b88ecc7e228da6caa04e1cc264c036a
jamanbala_it <ermekoffdastan@yandex.ru>
2022-12-15 08:56:37 -0600

    add static settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 STATIC_ROOT = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles"),]
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
