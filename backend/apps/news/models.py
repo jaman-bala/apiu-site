fc400ade324ded1a31a4b70e31b395ea631080fc
Dastan <ermekoffdastan@gmail.com>
2022-12-16 10:00:10 +0600

    setting add static

--- backend/apps/news/models.py
+++ backend/apps/news/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Articles(models.Model):
-    title = models.CharField('Название', max_length=50)
-    anons = models.CharField('Анонс', max_length=250)
+    title = models.CharField('Название', max_length=70)
+    anons = models.CharField('Анонс', max_length=250, blank=True, null=True)
     full_text = models.TextField('Статья')
     img = models.ImageField(upload_to='img', blank=True, null=True)
     specs = models.FileField(upload_to='specs', blank=True, null=True)
