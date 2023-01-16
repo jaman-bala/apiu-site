d5eaddc03ff7cbd399243ff38f641733a3e1c91e
Dastan <ermekoffdastan@gmail.com>
2023-01-16 14:12:22 +0600

    add new info

--- backend/apps/cloude/additional.py
+++ backend/apps/cloude/additional.py
@@ -2,15 +2,13 @@ from django.db import models
 
 class Info(models.Model):
     title = models.CharField('Название', max_length=10, blank=True)
-    text = models.TextField('Текст', blank=True)
-    photo = models.ImageField(upload_to='img', blank=True)
     video = models.FileField(upload_to='videos_uploaded',null=True, blank=True)
     
 
     def __str__(self):
-        return str(self.id)
+        return str(self.title)
 
 
     class Meta:
-        verbose_name = 'Информация'
-        verbose_name_plural = 'Добавить информацию'
+        verbose_name = 'Видео'
+        verbose_name_plural = 'Добавить видео'
--- backend/apps/cloude/views.py
+++ backend/apps/cloude/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from .models import Photo
 from .additional import Info
-
+from ..news.models import Articles
     
 
 def index(request):
     informations = Info.objects.all()
     photo = Photo.objects.all()
+    news = Articles.objects.all()
     context = {
         "photo":photo,
-        "informations":informations
+        "informations":informations,
+        "news": news,
     }
     return render(request, 'main/index.html', context)
 
