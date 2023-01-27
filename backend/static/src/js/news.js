6acc50db01bc1cea6431c62f2fabd35070c9baea
Dastan <ermekoffdastan@gmail.com>
2023-01-27 19:10:39 +0600

    edit js swiper

--- backend/static/src/js/news.js
+++ backend/static/src/js/news.js
@@ -1,34 +1,47 @@
-let loadMoreBtn = document.querySelector("#load-more");
-let currentItem = 3;
+// let loadMoreBtn = document.querySelector("#load-more");
+// let currentItem = 3;
 
-loadMoreBtn.onclick = () => {
-  let boxes = [
-    ...document.querySelectorAll(".container__news .box-container .box"),
-  ];
-  for (var i = currentItem; i < currentItem + 3; i++) {
-    boxes[i].style.display = "inline-block";
-  }
-  currentItem += 3;
+// loadMoreBtn.onclick = () => {
+//   let boxes = [
+//     ...document.querySelectorAll(".container__news .box-container .box"),
+//   ];
+//   for (var i = currentItem; i < currentItem + 3; i++) {
+//     boxes[i].style.display = "inline-block";
+//   }
+//   currentItem += 3;
 
-  if (currentItem >= boxes.length) {
-    loadMoreBtn.style.display = "none";
-  }
-};
+//   if (currentItem >= boxes.length) {
+//     loadMoreBtn.style.display = "none";
+//   }
+// };
 
 
-var swiper = new Swiper(".slider-content", {
+var swiper = new Swiper(".slide-content", {
   slidesPerView: 3,
-  spaceBetween: 30,
-  slidesPerGroup: 3,
+  spaceBetween: 25,
   loop: true,
-  loopFillGroupWithBlank: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    dynamicBullets: true,
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  }
 });
 
