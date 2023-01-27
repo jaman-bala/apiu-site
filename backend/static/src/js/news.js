60e44316c11ed3c9f3916e4c0478c7807497abd8
Dastan <ermekoffdastan@gmail.com>
2023-01-27 18:15:34 +0600

    add new js and css swiper

--- backend/static/src/js/news.js
+++ backend/static/src/js/news.js
@@ -14,3 +14,21 @@ loadMoreBtn.onclick = () => {
     loadMoreBtn.style.display = "none";
   }
 };
+
+
+var swiper = new Swiper(".slider-content", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
