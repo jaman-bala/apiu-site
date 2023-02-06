25ce010f4155d7a5524283110db55c7303a68844
Dastan <ermekoffdastan@gmail.com>
2023-02-06 14:37:10 +0600

    add pagination

--- backend/apps/gallery/views.py
+++ backend/apps/gallery/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Gallery
+from django.db.models import Q
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, ListView
 
 
@@ -9,6 +11,16 @@ class GalleryView(ListView):
         context_object_name = 'gallery'
         paginate_by = 100
         queryset = Gallery.objects.all()
+
+        def get_queryset(self):  # new
+                query = self.request.GET.get("q")
+                if query:
+                        object_list = Gallery.objects.filter(
+                                Q(country__icontains=query) | Q(title__icontains=query) | Q(date__icontains=query)
+                        )
+                else:
+                        object_list = Gallery.objects.all()
+                return object_list
 
 
 class GalleryDetailView(DetailView):
--- backend/apps/news/views.py
+++ backend/apps/news/views.py
@@ -6,7 +6,7 @@ class NewshomeView(ListView):
         model = Articles
         template_name = 'news/news_home.html'
         context_object_name = 'news'
-        paginate_by = 2
+        paginate_by = 10
         queryset = Articles.objects.all()
 
 
