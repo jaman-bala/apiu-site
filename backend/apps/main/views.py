c515539ac9618edade80b1c5b2c0fc9c92d53cda
Dastan <ermekoffdastan@gmail.com>
2023-02-07 17:32:47 +0600

    new info

--- backend/apps/main/views.py
+++ backend/apps/main/views.py
@@ -3,17 +3,20 @@ from .models import Photo
 from .staff import Staff
 from .additional import Video
 from ..news.models import Articles
+from ..gallery.models import Gallery
 from backend.apps.news.models import Articles
     
 
 def index(request):
-    video = Video.objects.all()
+    video = Video.objects.all()[:3]
+    gallery = Gallery.objects.all()[:5]
     photos = Photo.objects.all()
     news = Articles.objects.all()        # если добавить то только выйдет 3 информации[:3]
     context = {
         "photos":photos,
         "video":video,
         "news": news,
+        "gallery": gallery,
     }
     return render(request, 'main/index.html', context)
 
