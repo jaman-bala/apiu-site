e41baa88412d3217d2c7c4904e9ade5fa5766045
Dastan <ermekoffdastan@gmail.com>
2023-02-09 17:24:07 +0600

    edit new info

--- backend/apps/tender/models.py
+++ backend/apps/tender/models.py
@@ -2,12 +2,11 @@ from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
 
 class Tender(models.Model):
-    country = models.CharField("Страна",  max_length=250)
     title = models.CharField("Название проекта", max_length=250)
     title_contracts = models.CharField("Название контракта", max_length=250)
-    finance = models.CharField("Финансирование", max_length=250)
     pricath = models.CharField("Номер приказа", max_length=250)
     text = RichTextUploadingField("Текст")
+    file = models.FileField(upload_to='files', blank=True, verbose_name="Добавить файл")
     date = models.DateTimeField('Срок подачи заявок')
     created = models.DateTimeField(verbose_name="Дата создание", auto_now_add=True)
     updated = models.DateTimeField(verbose_name="Дата обновления", auto_now=True)
