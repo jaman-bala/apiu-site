a230d81ebc1494b6eeadf302a39cdba6c07bade2
Dastan <ermekoffdastan@gmail.com>
2023-02-10 10:23:21 +0600

    add new css

--- backend/apps/tender/vacancy.py
+++ backend/apps/tender/vacancy.py
@@ -6,6 +6,7 @@ class Vacancy(models.Model):
     country = models.CharField("Страна",  max_length=250)
     title = models.CharField("Название проекта", max_length=250)
     text = RichTextUploadingField("Текст")
+    file = models.FileField(upload_to='files', blank=True, verbose_name="Добавить файл")
     is_active = models.BooleanField("Активный", default = True)
     date = models.DateTimeField('Срок подачи заявок')
     created = models.DateTimeField(verbose_name="Дата создание", auto_now_add=True)
