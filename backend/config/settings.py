769985492f79bcbf3da58b2ab026889586f6d052
Dastan <ermekoffdastan@gmail.com>
2023-02-15 16:36:48 +0600

    edit static urls

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -144,7 +144,8 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, "locale")]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    BASE_DIR / "static",
+    '/opt/apiu/backend/static/',
 ]
 
 
