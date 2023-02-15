2884fe1ddb6210a37943f751a7632bda50e80005
Dastan <ermekoffdastan@gmail.com>
2023-02-15 17:06:54 +0600

    edit static urls

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -142,7 +142,7 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, "locale")]
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
