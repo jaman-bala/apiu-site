bcc2b2bc738b694cbfde7d03705bbfacfc5c7295
Dastan <ermekoffdastan@gmail.com>
2023-02-16 15:48:12 +0600

    edit settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,9 +10,9 @@ from easy_thumbnails.conf import Settings as thumbnail_settings
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-=@0k%^7cdpu7cq9=yu16&(2t-xqa$(6)gehker8awr@*2b03t'
 
-DEBUG = False
-ALLOWED_HOSTS = ["apiu.kg"]
-CSRF_TRUSTED_ORIGINS = ['https://apiu.kg']
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
+# CSRF_TRUSTED_ORIGINS = ['https://apiu.kg']
 
 # Application definition
 INSTALLED_APPS = [
