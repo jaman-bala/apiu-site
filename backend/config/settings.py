7eccfec8074e5d88f8f1d5e98a831d8db2cf158f
Dastan <ermekoffdastan@gmail.com>
2023-02-16 09:53:11 +0600

    edit static fils

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -142,7 +142,8 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, "locale")]
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/opt/apiu/backend/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
