58efebd3d54477817e99413f50f299449f006d36
Dastan <ermekoffdastan@gmail.com>
2023-03-13 16:01:19 +0600

    edit settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -142,7 +142,7 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, "locale")]
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'src/static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
