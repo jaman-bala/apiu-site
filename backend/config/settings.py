5445fd9781b4720a75ff7167adfce49fcb48c50e
Dastan <ermekoffdastan@gmail.com>
2023-03-14 11:39:25 +0600

    edit css color

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.config.urls'
@@ -140,6 +141,7 @@ global_settings.LANGUAGES = global_settings.LANGUAGES + [("ky", 'Кыргызч�
 LOCALE_PATHS = [os.path.join(BASE_DIR, "locale")]
 
 # Static files (CSS, JavaScript, Images)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/opt/apiu/backend/static/'
