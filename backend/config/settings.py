e57536aca8417d4a59240850a1633271f41fa9bb
Dastan <ermekoffdastan@gmail.com>
2023-05-17 10:30:49 +0600

    'settings'

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -73,23 +73,23 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.config.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.getenv("POSTGRES_DB",default='apiu_db'),
-#         'USER' : os.getenv("POSTGRES_USER",default='apiu_superuser'),
-#         'PASSWORD' : os.getenv("POSTGRES_PASSWORD",default='ZAQ!@#$%tgb'),
-#         'HOST' : os.getenv("POSTGRES_HOST",default='127.0.0.1'),
-#         'PORT' : os.getenv("POSTGRES_PORT", default='5432'),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("POSTGRES_DB",default='apiu_db'),
+        'USER' : os.getenv("POSTGRES_USER",default='apiu_superuser'),
+        'PASSWORD' : os.getenv("POSTGRES_PASSWORD",default='ZAQ!@#$%tgb'),
+        'HOST' : os.getenv("POSTGRES_HOST",default='127.0.0.1'),
+        'PORT' : os.getenv("POSTGRES_PORT", default='5432'),
+    }
+}
 
 
 # Password validation
@@ -141,15 +141,13 @@ global_settings.LANGUAGES = global_settings.LANGUAGES + [("ky", 'Кыргызч�
 LOCALE_PATHS = [os.path.join(BASE_DIR, "locale")]
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 # Static files (CSS, JavaScript, Images)
-# STATIC_URL = '/static/'
-# if DEBUG:
-#     STATIC_ROOT = os.path.join(BASE_DIR, '/static')
-# else:
-#     STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = '/static/'
+if DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+else:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # MEDIA
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -157,15 +155,15 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # STOREGE
-# DEFAULT_FILE_STORAGE = "minio_storage.storage.MinioMediaStorage"
-# MINIO_STORAGE_ENDPOINT = 'http://base.apiu.kg:8080'
-# MINIO_STORAGE_ACCESS_KEY = 's3igtWTcQqq2uyp7'
-# MINIO_STORAGE_SECRET_KEY = 'DozBIJDceDimmGHLwUaMDiX8j13bF2oM'
-# MINIO_STORAGE_USE_HTTPS = False
-# MINIO_STORAGE_MEDIA_BUCKET_NAME = 'media'
-# MINIO_STORAGE_AUTO_CREATE_MEDIA_BUCKET = True
-# MINIO_STORAGE_STATIC_BUCKET_NAME = 'static'
-# MINIO_STORAGE_AUTO_CREATE_STATIC_BUCKET = True
+DEFAULT_FILE_STORAGE = "minio_storage.storage.MinioMediaStorage"
+MINIO_STORAGE_ENDPOINT = '10.11.11.8:9000'
+MINIO_STORAGE_ACCESS_KEY = 's3igtWTcQqq2uyp7'
+MINIO_STORAGE_SECRET_KEY = 'DozBIJDceDimmGHLwUaMDiX8j13bF2oM'
+MINIO_STORAGE_USE_HTTPS = False
+MINIO_STORAGE_MEDIA_BUCKET_NAME = 'media'
+MINIO_STORAGE_AUTO_CREATE_MEDIA_BUCKET = True
+MINIO_STORAGE_STATIC_BUCKET_NAME = 'static'
+MINIO_STORAGE_AUTO_CREATE_STATIC_BUCKET = True
 
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
